b0350676f52bfa9d4bcfff3286b1c05debba45c3
Ahmed Amine Doudech <hmidetocraft@gmail.com>
2021-03-13 22:47:02 +0100

    [+Commands] + queuelist

--- commands/music.js
+++ commands/music.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 let prefix = botconfig.prefix;
 
-const ytdl = require('ytdl-core')
+const getytid = require('get-youtube-id')
+const fetchVideoInfo = require('youtube-infofix')
+// const yt = require('youtube.get-video-info')
+// const getyttitle = require('get-youtube-title')
+
+const ytdl = require('ytdl-core');
 
 const streamOptions = {
     seek: 0,
@@ -55,8 +60,11 @@ module.exports.run = async (bot, message, args) => {
     
     else if (parameter == "skip")
     {
-        if (bot.dispatcher) bot.dispatcher.end()
-        else message.channel.send("Nothing is playing!")
+        if (bot.dispatcher) {
+            message.channel.send(":fast_forward: Skipping current song!")
+            bot.dispatcher.end()
+        }
+        else message.channel.send(":x: Nothing is playing!")
     }
 
     else if (parameter == "leave")
@@ -64,52 +72,36 @@ module.exports.run = async (bot, message, args) => {
         voiceChannel.leave()
     }
 
+    else if (parameter == "queuelist")
+    {
+        if(bot.musicUrls.length > 0)
+        {
+            let fieldMsg = bot.musicUrls.join('\n')
+            let embed = new Discord.RichEmbed();
+            embed.setColor("RANDOM")
+            embed.setThumbnail(bot.user.displayAvatarURL)
+            embed.setAuthor(bot.user.username, bot.user.displayAvatarUrl)
+            embed.addField('Queued songs : ', fieldMsg)
+            message.channel.send(embed)
+        } else {
+            message.channel.send(":clock10: Nothing left on the queue.")
+        }
+    }
+
     else {
         message.channel.send(`Can't find \`${parameter}\` in music parameters! please use \`${prefix}help music\` for more informations!`)
     }
 
-    // let url = args[0]
-    // let voiceChannel = message.member.voiceChannel
-    // console.log("Voice channel", voiceChannel)
-    
-    // if (!voiceChannel) return message.channel.send("You're not in a voice channel, please join a voice channel before playing music.")
-
-
-    // if (ytdl.validateURL(url))
-    // {
-    //     console.log("Valid URL")
-    //     var flag = bot.musicUrls.some(element => element === url);
-    //     if (!flag) 
-    //     {
-    //         bot.musicUrls.push(url)
-    //         if(voiceChannel != null)
-    //         {
-    //             if (voiceChannel.connection)
-    //             {
-    //                 const embed = new Discord.RichEmbed();
-    //                 embed.setAuthor(bot.user.username, bot.user.displayAvatarUrl);
-    //                 embed.setDescription("You've successfully added url to queue!");
-    //                 message.channel.send(embed)
-    //             } else {
-    //                 try 
-    //                 {
-    //                     const voiceConnection = await voiceChannel.join()
-    //                     await playSong(bot, message.channel, voiceConnection, voiceChannel)
-    //                 } catch(ex)
-    //                 {
-    //                     console.log(ex)
-    //                 }
-    //             }
-    //         } 
-    //     }
-    // } else 
-    // {
-    //     message.channel.send("Youtube URL is not correct, please check the URL before playing any audio.")
-    // }
-
 }
 
 async function playSong(bot, messageChannel, voiceConnection, voiceChannel) {
+    let embed = new Discord.RichEmbed();
+    embed.setColor("RANDOM")
+    embed.setThumbnail(bot.user.displayAvatarURL)
+    embed.setAuthor(bot.user.username, bot.user.displayAvatarUrl)
+    embed.addField('Current song : ', bot.musicUrls[0])
+    messageChannel.send(embed)
+
     const stream = ytdl(bot.musicUrls[0], {filter: 'audioonly'})
     bot.dispatcher = voiceConnection.playStream(stream, streamOptions)
 
@@ -129,7 +121,7 @@ async function playSong(bot, messageChannel, voiceConnection, voiceChannel) {
 module.exports.config = {
     name: "music", // you can you this as the original command
     aliases: ["m", "audio"], // or you can do it with multiple commands (try to type it with prefix)
-    usage: `\n${prefix}music play <youtube URL> \n ${prefix}music skip\n${prefix}music leave`,
+    usage: `\n${prefix}music play <youtube URL> \n ${prefix}music skip\n${prefix}music leave\n${prefix}music queue`,
     description: "Use this command to play audio in voice channel",
     accessableby: "Members"
 }
